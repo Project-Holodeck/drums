dcf7d16e144f43bccba406df419222d0fda76a17
ailersic <ailersic@users.noreply.github.com>
2020-02-05 18:55:46 -0500

    Added drum hit timing

--- TEST-2D/Assets/Snakedestroy.cs
+++ TEST-2D/Assets/Snakedestroy.cs
@@ -13,11 +13,5 @@ public class Snakedestroy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        if (this.transform.position.y >= 20 && this.transform.position.y <= 25)
-        {
-            //Destroy(gameObject);
-        }
-
     }
 }
